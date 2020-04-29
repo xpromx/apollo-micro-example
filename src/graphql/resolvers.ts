7003a09335c2982ef5daf9f3b89af50400ffaa6c
xpromx <xpromx@gmail.com>
2020-04-29 14:21:20 +0900

    fix(now.json): rollback includeFiles

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -2,10 +2,7 @@ import path from "path"
 import { fileLoader, mergeResolvers } from "merge-graphql-schemas"
 
 const resolversArray = [
-  ...fileLoader(path.join(__dirname, "../**/*Resolvers.ts")),
-  ...fileLoader(path.join(__dirname, "../**/*Resolvers.js")),
-  ...fileLoader(path.join(__dirname, "/src/**/*Resolvers.ts")),
-  ...fileLoader(path.join(__dirname, "../src/**/*Resolvers.ts"))
+  ...fileLoader(path.join(__dirname, "../**/*Resolvers.ts"))
 ]
 
 const resolvers = mergeResolvers(resolversArray)
