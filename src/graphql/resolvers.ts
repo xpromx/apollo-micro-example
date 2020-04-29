c4ff6b7038a8b28adba119e9aeeafb0ef9554365
xpromx <xpromx@gmail.com>
2020-04-29 13:09:55 +0900

    load resolvers

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -2,7 +2,10 @@ import path from "path"
 import { fileLoader, mergeResolvers } from "merge-graphql-schemas"
 
 const resolversArray = [
-  ...fileLoader(path.join(__dirname, "../**/*Resolvers.ts"))
+  ...fileLoader(path.join(__dirname, "../**/*Resolvers.ts")),
+  ...fileLoader(path.join(__dirname, "../**/*Resolvers.js")),
+  ...fileLoader(path.join(__dirname, "/src/**/*Resolvers.ts")),
+  ...fileLoader(path.join(__dirname, "../src/**/*Resolvers.ts"))
 ]
 
 const resolvers = mergeResolvers(resolversArray)
